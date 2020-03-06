f5a3f1631a27cf84205eff815e3a5c73bebfc636
Vincent Roquelaure <roquelaurevincent@gmail.com>
2020-03-06 14:43:20 +0100

    [REFACTOR] should_print_buzz_if_a_multiple_of_5

--- src/main/java/pro/roquelaure/fizzbuzz/FizzBuzz.java
+++ src/main/java/pro/roquelaure/fizzbuzz/FizzBuzz.java
@@ -6,14 +6,6 @@ public class FizzBuzz {
     }
 
     public static String calculate(int num) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            return "fizzbuzz";
-        } else if (num % 3 == 0) {
-            return "fizz";
-        } else if (num % 5 == 0) {
-            return "buzz";
-        } else {
-            return "";
-        }
+        return num % 3 == 0 ? (num % 5 == 0 ? "fizzbuzz" : "fizz") : num % 5 == 0 ? "buzz" : "";
     }
 }
